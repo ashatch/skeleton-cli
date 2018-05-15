2687ce5257e7aa608cafa090e5b1f8c56e99554c
Andrew Hatch <ahatch@amplience.com>
2018-05-15 10:49:57 +0100

    numeric types test

--- src/test/java/net/andrewhatch/skeletoncli/ArgumentResolverTest.java
+++ src/test/java/net/andrewhatch/skeletoncli/ArgumentResolverTest.java
@@ -40,6 +40,25 @@ public class ArgumentResolverTest {
     assertThat(parameters.isPresent()).isFalse();
   }
 
+  @Test
+  public void numeric_parameters() {
+    final ArgumentResolver<ParametersNumericTypes> resolver = new ArgumentResolver<>(ParametersNumericTypes.class);
+    int integerArgument = 314159;
+    float floatArgument = 3.14159f;
+    double doubleArgument = 3.141;
+    final String[] args = {
+        "--integer", String.valueOf(integerArgument),
+        "--floatType", String.valueOf(floatArgument),
+        "--doubleType", String.valueOf(doubleArgument)
+    };
+
+    Optional<ParametersNumericTypes> parameters = resolver.resolve(args);
+    assertThat(parameters.isPresent()).isTrue();
+    assertThat(parameters.get().getInteger()).isEqualTo(integerArgument);
+    assertThat(parameters.get().getFloatType()).isEqualTo(floatArgument);
+    assertThat(parameters.get().getDoubleType()).isEqualTo(doubleArgument);
+  }
+
   public static class NoParameters {
   }
 
@@ -52,6 +71,36 @@ public class ArgumentResolverTest {
 
     public void setMessage(String message) {
       this.message = message;
+    }
+  }
+
+  public static class ParametersNumericTypes {
+    private int integer;
+    private float floatType;
+    private double doubleType;
+
+    public int getInteger() {
+      return integer;
+    }
+
+    public void setInteger(int integer) {
+      this.integer = integer;
+    }
+
+    public float getFloatType() {
+      return floatType;
+    }
+
+    public void setFloatType(float floatType) {
+      this.floatType = floatType;
+    }
+
+    public double getDoubleType() {
+      return doubleType;
+    }
+
+    public void setDoubleType(double doubleType) {
+      this.doubleType = doubleType;
     }
   }
 }
